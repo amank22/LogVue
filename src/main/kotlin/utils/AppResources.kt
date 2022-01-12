20b90a4e7ab06292faa76cb2c48e35f4dd9d6d9b
aman.kapoor <aman.kapoor@go-mmt.com>
2022-01-13 00:52:37 +0530

    Capability to turn-off indexing parameter attributes #45

--- src/main/kotlin/utils/AppResources.kt
+++ src/main/kotlin/utils/AppResources.kt
@@ -5,6 +5,7 @@ interface StringRes {
     val appName: String
     val filterFaqTitle: String
     val appCrashText: String
+    val turnOffFilterIndexText: String
 }
 
 class EnglishStringRes : StringRes {
@@ -13,5 +14,7 @@ class EnglishStringRes : StringRes {
     override val appCrashText: String = "Unfortunately the app was crashed last time. " +
             "While we look into this, you can also report this issue to us on github or through " +
             "mail describing the scenario that caused this crash."
+    override val turnOffFilterIndexText: String = "If you are facing repeated issues with filter, " +
+            "click here and re-run query for filter. This is one time setting per session"
 
 }
---
+++ src/main/kotlin/utils/ConfigConstants.kt
@@ -0,0 +1,7 @@
+package utils
+
+object ConfigConstants {
+
+    const val QUERY_INDEX = "indexQueryParams"
+
+}
