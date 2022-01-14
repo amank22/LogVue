8e778b7b2a2335ca9126f7205beb0aa0bf495b33
aman.kapoor <aman.kapoor@go-mmt.com>
2022-01-14 16:44:56 +0530

    Dialog performance improvement #32, version in gradle

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -3,8 +3,8 @@ package app
 import androidx.compose.ui.window.application
 
 fun main() = application(false) {
-    val environment = System.getenv("SKIKO_RENDER_API")
-    val property = System.getProperty("skiko.renderApi")
-    println("env: $environment render: $property")
+//    val environment = System.getenv("SKIKO_RENDER_API")
+//    val property = System.getProperty("skiko.renderApi")
+//    println("env: $environment render: $property")
     appWindow()
 }
