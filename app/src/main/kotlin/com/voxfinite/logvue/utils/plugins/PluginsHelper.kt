3cac9b3daa8d2e11e0a260908289931b641dc8d8
aman.kapoor <aman.kapoor@go-mmt.com>
2022-02-10 12:11:59 +0530

    Plugin support was broken. Fixed.

--- app/src/main/kotlin/com/voxfinite/logvue/utils/plugins/PluginsHelper.kt
+++ app/src/main/kotlin/com/voxfinite/logvue/utils/plugins/PluginsHelper.kt
@@ -1,17 +1,19 @@
-package com.voxfinite.logvue.utils
+package com.voxfinite.logvue.utils.plugins
 
 import com.voxfinite.logvue.api.LogEventParser
 import com.voxfinite.logvue.parsers.FirebaseParser
 import com.voxfinite.logvue.storage.StorageHelper
+import com.voxfinite.logvue.utils.AppLog
 import org.pf4j.CompoundPluginDescriptorFinder
 import org.pf4j.DefaultPluginManager
 import org.pf4j.ManifestPluginDescriptorFinder
 import org.pf4j.PropertiesPluginDescriptorFinder
 import java.nio.file.Path
+import kotlin.jvm.Throws
 
 object PluginsHelper {
 
-    private class PluginManager(importPaths: List<Path>) : DefaultPluginManager(importPaths) {
+    internal class PluginManager(importPaths: List<Path>) : DefaultPluginManager(importPaths) {
         override fun createPluginDescriptorFinder(): CompoundPluginDescriptorFinder {
             return CompoundPluginDescriptorFinder() // Demo is using the Manifest file
                 // PropertiesPluginDescriptorFinder is commented out just to avoid error log
@@ -20,7 +22,7 @@ object PluginsHelper {
         }
     }
 
-    private val pluginManager by lazy {
+    internal val pluginManager by lazy {
         val pluginsPath = StorageHelper.getPluginsPath()
         AppLog.d(pluginsPath.toString())
         PluginManager(listOf(pluginsPath))
@@ -33,7 +35,6 @@ object PluginsHelper {
     fun load() {
         // load the plugins
         pluginManager.loadPlugins()
-
         // enable a disabled plugin
         // pluginManager.enablePlugin("welcome-plugin")
 
@@ -47,6 +48,21 @@ object PluginsHelper {
         allParsers.addAll(getDefaultParsers())
         allParsers.addAll(parsers)
         return allParsers
+    }
+
+    @Throws(ClassCastException::class)
+    fun tryResolveClass(className : String) : Class<*> {
+        pluginManager.resolvedPlugins.forEach {
+            val classLoader = it.pluginClassLoader
+            val resolvedClass: Class<*>?
+            try {
+                resolvedClass = Class.forName(className, false, classLoader)
+                return resolvedClass
+            } catch (ignore : ClassNotFoundException) {
+                // ignore
+            }
+        }
+        return Class.forName(className, false, Thread.currentThread().contextClassLoader)
     }
 
     private fun getDefaultParsers(): MutableList<LogEventParser> = arrayListOf(FirebaseParser())
